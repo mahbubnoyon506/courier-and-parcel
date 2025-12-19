c1d46c624716f1ebe4a120137d49337b50d9b53c
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-12-19 15:00:33 +0600

    Default port issue fixed for render

--- server/src/index.js
+++ server/src/index.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
